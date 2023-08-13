ac18f33a2dbd55bd21cc91a6556bd48ca476bb1a
Mai <21039366+Mai0313@users.noreply.github.com>
2023-08-13 15:56:07 +0800

    新增直接從網址取圖

--- get_captcha.py
+++ get_captcha.py
@@ -8,74 +8,75 @@ import requests
 import urllib
 from omegaconf import OmegaConf
 
+def save_image(output_path, captcha_code, screenshot: bytes):
+    with open(f'{output_path}/{captcha_code}.png', 'wb') as f:
+        f.write(screenshot)
 
 def get_captcha_to_database(website: str, target_element: str, output_path: str, version: int) -> str:
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context(
-            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-            java_script_enabled=True,
-            accept_downloads=False,
-            has_touch=False,
-            is_mobile=False,
-            locale='en-US',
-            permissions=[],
-            geolocation=None,
-            color_scheme='light',
-            timezone_id='Asia/Shanghai'
-        )
-        page = context.new_page()
+    if not target_element:
+        # 除非可以用網址直接打開圖片，不然不建議用這個版本
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
+        full_url = website
+        response = requests.get(url = full_url, headers=headers)
+        base64_screenshot = base64.b64encode(response.content).decode("utf-8")
+    else:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            context = browser.new_context(
+                user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                java_script_enabled=True,
+                accept_downloads=False,
+                has_touch=False,
+                is_mobile=False,
+                locale='en-US',
+                permissions=[],
+                geolocation=None,
+                color_scheme='light',
+                timezone_id='Asia/Shanghai'
+            )
+            page = context.new_page()
 
-        # 進入網站
-        page.goto(website)
+            # 進入網站
+            page.goto(website)
 
-        if version == 1:
-            # 直接截圖
-            captcha_element = page.wait_for_selector(target_element)
-            screenshot = captcha_element.screenshot()
-            base64_screenshot = base64.b64encode(screenshot).decode("utf-8")
+            if version == 1:
+                # 直接截圖
+                captcha_element = page.wait_for_selector(target_element)
+                screenshot = captcha_element.screenshot()
+                base64_screenshot = base64.b64encode(screenshot).decode("utf-8")
 
-        elif version == 2:
-            # 找到圖片位置再截圖，跟直接截圖差不多；不穩定，但有時可用
-            screenshot_element = page.query_selector(target_element)
-            bounding_box = screenshot_element.bounding_box()
-            screenshot = page.screenshot(clip=bounding_box)
-            base64_screenshot = base64.b64encode(screenshot).decode("utf-8")
+            elif version == 2:
+                # 找到圖片位置再截圖，跟直接截圖差不多；不穩定，但有時可用
+                screenshot_element = page.query_selector(target_element)
+                bounding_box = screenshot_element.bounding_box()
+                screenshot = page.screenshot(clip=bounding_box)
+                base64_screenshot = base64.b64encode(screenshot).decode("utf-8")
 
-        elif version == 3:
-            # 直接找圖片的src
-            captcha_element = page.wait_for_selector(target_element)
-            screenshot = captcha_element.get_attribute('src')
-            if screenshot.startswith('data:image'):
-                base64_screenshot = screenshot.split(',')[1]
-            else:
-                base_url = page.url
-                full_url = urllib.parse.urljoin(base_url, screenshot)
-                headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
-                response = requests.get(url = full_url, headers=headers)
-                base64_screenshot = base64.b64encode(response.content).decode("utf-8")
-        
-        elif version == 4:
-            # 除非可以用網址直接打開圖片，不然不建議用這個版本
-            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
-            img_src = "https://www.newebpay.com/main/main/captcha_img"
-            response = requests.get(url = img_src, headers=headers)
-            base64_screenshot = base64.b64encode(response.content).decode("utf-8")
+            elif version == 3:
+                # 直接找圖片的src
+                captcha_element = page.wait_for_selector(target_element)
+                screenshot = captcha_element.get_attribute('src')
+                if screenshot.startswith('data:image'):
+                    base64_screenshot = screenshot.split(',')[1]
+                else:
+                    base_url = page.url
+                    full_url = urllib.parse.urljoin(base_url, screenshot)
+                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
+                    response = requests.get(url = full_url, headers=headers)
+                    base64_screenshot = base64.b64encode(response.content).decode("utf-8")
 
-        # 調用解析驗證碼的函數
-        captcha_code = CaptchaResolver().get_captcha_code(base64_screenshot)
+            browser.close()
 
-        # base64 -> bytes
-        screenshot = base64.b64decode(base64_screenshot)
-        # if isinstance(screenshot, str):
-        #     screenshot = base64.b64decode(base64_screenshot)
+    # 調用解析驗證碼的函數
+    captcha_code = CaptchaResolver().get_captcha_code(base64_screenshot)
 
-        # bytes -> image
-        with open(f'{output_path}/{captcha_code}.png', 'wb') as f:
-            f.write(screenshot)
+    # base64 -> bytes
+    screenshot = base64.b64decode(base64_screenshot)
 
-        browser.close()
-        return captcha_code
+    # bytes -> image
+    save_image(output_path, captcha_code, screenshot)
+
+    return captcha_code
 
 if __name__ == "__main__":
     today = datetime.datetime.now().strftime("%Y%m%d")
