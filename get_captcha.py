6c7e9e449b64e39dcbc0b4899cfbac141a5199ac
Wei <21039366+Mai0313@users.noreply.github.com>
2023-08-22 18:26:11 +0000

    更改至綠界

--- get_captcha.py
+++ get_captcha.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     push_frequency = cfg.push_frequency
 
     # Select the Website
-    target_website = cfg.thsrc
+    target_website = cfg.ecpay
 
     website_name = target_website.website_name
     website_url = target_website.website_url
